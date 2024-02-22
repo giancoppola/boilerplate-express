c3305b2aa95d41809da2a1d533d0f41b93fb83a4
Gian <giancoppola@hotmail.co.uk>
2024-02-22 15:50:13 +0000

    fcc

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 let express = require('express');
+let bodyParser = require('body-parser')
 let app = express();
 
 function ToMessageStyle(str){
@@ -15,6 +16,7 @@ function ToMessageStyle(str){
 app.use("/public", express.static(__dirname + "/public"))
 
 // Root
+app.use("/", bodyParser.urlencoded({extended: false}));
 app.use("/", (req, res, next) => {
     console.log(req.method+" "+req.path+" - "+req.ip);
     next();
@@ -45,5 +47,24 @@ app.get("/:word/echo", (req, res) => {
         echo: req.params.word
     })
 })
+
+app.use("/name", bodyParser.urlencoded({extended: false}))
+app.route("/name")
+    .get((req, res) => {
+        console.log(req.query);
+        let first = req.query.first != null ? req.query.first : "";
+        let last = req.query.last != null ? req.query.last : "";
+        res.json({
+            name: `${first} ${last}`
+        })
+    })
+    .post((req, res) => {
+        console.log(req.body);
+        let first = req.body.first != null ? req.body.first : "";
+        let last = req.body.last != null ? req.body.last : "";
+        res.json({
+            name: `${first} ${last}`
+        })
+    })
 
 module.exports = app;
